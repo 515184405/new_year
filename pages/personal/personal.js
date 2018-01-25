218322c0c7d6b0b880d5993238fbfa3e9ebc773b
515184405 <515184405@qq.com>
2018-01-25 14:53:53 +0800

    上传项目

---
+++ pages/personal/personal.js
@@ -0,0 +1,135 @@
+// pages/personal/personal.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    action : 1, //按钮状态
+    is_flicker : false, //是否显示录制icon
+    timer : null, //定时器开关
+    camera_show : 0, //是否开始录像
+    uid : null,
+  },
+  flicker:function(){
+    var that = this;
+    this.data.timer = setInterval(function(){
+      that.setData({
+        'is_flicker': !that.data.is_flicker
+      })
+    },1000)
+  },
+  //录制视频方法
+  startRecord:function(){
+    var that = this;
+    this.setData({
+      'camera_show': 1,
+    })
+    const ctx = wx.createCameraContext();
+    ctx.startRecord({
+       success:res => {
+         that.flicker();
+         that.setData({
+            'action' : 2,
+          })
+       },
+       error:res => {
+          console.log(res);
+       }
+    })
+  },
+  //结束录制方法
+  stopRecord:function(){
+    const ctx = wx.createCameraContext();
+    ctx.stopRecord({
+      success:res => {
+        clearInterval(this.data.timer);
+        this.setData({
+          'action': 3,
+          'camera_show': 2,
+          'is_flicker': false,
+          'video_img': res.tempThumbPath,
+          'video_url': res.tempVideoPath,
+        })
+      }
+    })
+  },
+  // 视频播放按钮
+  plays:function(){
+    var video = wx.createVideoContext('myVideo');
+    video.play();
+  },
+  //视频停止按钮
+  pauses:function(){
+    var video = wx.createVideoContext('myVideo');
+    video.pause();
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function (res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: '我的贺岁视频',
+      path: '/page/personal/personal',
+      success: function (res) {
+        // 转发成功
+      },
+      fail: function (res) {
+        // 转发失败
+      }
+    }
+  }
+})
